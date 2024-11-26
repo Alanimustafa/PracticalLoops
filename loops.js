de8961a192623ec8442cae170084fd1230269c3c
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-11-26 12:20:01 -0600

    308-3-1 : Part 1: Loop 1 to 100

---
+++ loops.js
@@ -0,0 +1,34 @@
+// ALAB 308.3.1: Practical Loops
+//------------------------------
+
+
+
+//Part 1: Fizz Buzz
+
+//Loop through all numbers from 1 to 100.
+function loop1To100 () {
+    for (i = 1; i <= 100; i++) {
+      console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------- Functions Run -----------------------------
+
+//Loop through all numbers from 1 to 100.
+console.log('loop 1 to 100')
+loop1To100();
+console.log("--------------------------------------")
+
